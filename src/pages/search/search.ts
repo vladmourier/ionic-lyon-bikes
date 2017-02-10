7fa153c38864ead339eae594c98b76737278bb83
Vlad <vladmourier@hotmail.fr>
2017-02-10 18:16:14 +0100

    Optimisation du chargement des stations

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -2,14 +2,18 @@ import {Component} from '@angular/core';
 import {NavController} from "ionic-angular";
 import {StationWorker} from "../../app/StationWorker";
 import {HomePage} from "../home/home";
+import {Station} from "../../model/station/Station";
+
 
 @Component({
   selector: 'page-search',
   templateUrl: 'search.html',
-  providers: []
+  providers: [],
 })
-export class SearchPage extends StationWorker{
+export class SearchPage extends StationWorker {
   searchedValue: string;
+  cursor: number = 20;
+  displayedStations: Station[] = [];
 
   constructor(public navController: NavController) {
     super(navController);
@@ -17,21 +21,52 @@ export class SearchPage extends StationWorker{
 
   ngOnInit() {
     this.initStations();
+    for (let i = 0; i < this.cursor; i++)
+      this.displayedStations.push(this.stations[i]);
   }
 
   getItems(event) {
     this.searchedValue = event.target.value;
     this.initStations();
+    this.cursor = 20;
     if (this.searchedValue && this.searchedValue.trim() != '') {
       let regex = new RegExp(this.searchedValue, 'i');
-      this.stations = this.stations.filter((station) => regex.test(station.name) || regex.test(station.commune));
+      this.displayedStations = this.stations.filter((station) => regex.test(station.name) || regex.test(station.commune));
     }
   }
 
   jumpToStation(event) {
-    let clickedStationName = event.target.childNodes[1].innerHTML.trim();
+    let clickedStationName = event.target.innerText.trim();
     let regex = new RegExp(clickedStationName, 'i');
-    HomePage.stationToGo = this.stations.find((station) => regex.test(station.name));
+    HomePage.stationToGo = this.stations
+      .find((station) => regex.test(station.name));
     this.navController.parent.select(0);
+  }
+
+  doInfinite(infiniteScroll) {
+    var self = this;
+    let stationList = document.getElementById("stationsList");
+    let oldCursor = this.cursor;
+    this.cursor += 20;
+    for (let i = oldCursor; i < this.cursor; i++) {
+      let station = this.stations[i];
+      let stationHTML = document.createElement('ion-item');
+      stationHTML.className = "item-block item item-md my-dir";
+      stationHTML.addEventListener('click', (event: any) => {
+        let clickedStationName = event.target.innerText.trim();
+        let regex = new RegExp(clickedStationName, 'i');
+        HomePage.stationToGo = self.stations
+          .find((station) => regex.test(station.name));
+        self.navController.parent.select(0);
+      });
+      stationHTML.setAttribute("tappable", 'true');
+      stationHTML.innerHTML = '<ion-thumbnail item-left><img src="https://biok03.github.io/bi-velov/images/stations/' + station.number + '.jpg"></ion-thumbnail>' +
+        '<div class="item-inner"><div class="input-wrapper"><ion-label class="label label-md">'
+        + station.name + '</ion-label></div></div>';
+
+
+      stationList.appendChild(stationHTML)
+    }
+    infiniteScroll.complete();
   }
 }
